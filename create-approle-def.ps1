10740afeadb73a3891b39343bf57987321977f88
Krishna <krishna@Krishnas-MacBook-Pro.local>
2020-09-12 15:58:04 -0600

    Initial version.

---
+++ create-approle-def.ps1
@@ -0,0 +1,22 @@
+
+$roleDisplayName = Read-Host -Prompt 'Provide a user friendly name of the App-Role'
+$roleDescription = Read-Host -Prompt 'Provide a brief description of the App-Role'
+$newGUID         = New-Guid
+
+$output  = "{`n"
+$output += "`"allowedMemberTypes`": [`n"
+$output += "    `"User`"`n"
+$output += "],`n"
+$output += "`"description`": `""  + $roleDescription + "`",`n"
+$output += "`"displayName`": `""  + $roleDisplayName + "`",`n"
+$output += "`"id`": `""  + $newGUID + "`",`n"
+$output += "`"isEnabled`": true,`n"
+$output += "`"lang`": null,`n"
+$output += "`"origin`": `"Application`",`n"
+$output += "`"value`": `""  + $roleDisplayName.Replace(' ', '_') + "`"`n"
+$output += "}"
+
+Write-Output "`nThe following App-Role definition can be added to the App Manifest file in Azure AD:`n
+------------------------------------------------------------------------------------ `n`n$output`n" 
+
+
